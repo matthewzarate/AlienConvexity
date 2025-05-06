e9784a06a3e2cb72e00fef01e75cd3390f41ff41
Matthew Zárate <matthewzarate@Matthews-MacBook-Pro.local>
2025-05-05 20:33:47 -0700

    It's me

--- Matthew/src/GamePanel.java
+++ Matthew/src/GamePanel.java
@@ -2,4 +2,6 @@ import javax.swing.*;
 
 public class GamePanel extends JPanel {
     final int originalTileSize = 16;
+    //Traditional game screens had characters that were 16x16 size
+    //Therefore, scaling required
 }
